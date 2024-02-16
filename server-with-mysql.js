38b9e901a464603ed1ef28f8799f2e1a459d603a
Carlos Vertiz <vertizcar@gmail.com>
2024-02-16 10:50:32 -0300

    mysql table

--- server-with-mysql.js
+++ server-with-mysql.js
@@ -1,6 +1,4 @@
 import {createApp} from './start.js'
 import {ToughtModel} from './models/mysql/thoughs.js'
-import { createTable } from './models/mysql/createTable.js'
 
-createTable()
 createApp({thoughtModel:ToughtModel})
