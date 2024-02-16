a4f3c5ee23bd850fff50aa278503048e015c0e34
Carlos Vertiz <vertizcar@gmail.com>
2024-02-16 16:10:50 -0300

    start

--- routes/api.js
+++ routes/api.js
@@ -10,23 +10,23 @@ export const createThoughtRouter = ({thoughtModel}) => {
   const thoughtRouter = Router();
   const thoughtController = new ThoughtController({ thoughtModel })
 
-  // thoughtRouter.use(cors({
-  //   origin: (origin, callback) => {
-  //     console.log(origin)
-  //     const ACCEPTED_ORIGINS = [
-  //       'http://localhost:5173'
-  //     ]
-  //     if (ACCEPTED_ORIGINS.includes(origin)) {
-  //       return callback(null, true)
-  //     }
-  //     if (!origin){
-  //       return callback(null, true)
-  //     }
-  //     return callback(new Error('Not Allowed by CORS'))
-  //   }
+  thoughtRouter.use(cors({
+    origin: (origin, callback) => {
+      console.log(origin)
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:5173'
+      ]
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+      if (!origin){
+        return callback(null, true)
+      }
+      return callback(new Error('Not Allowed by CORS'))
+    }
 
-  // })
-  // )
+  })
+  )
 
   thoughtRouter.get('/', thoughtController.getAll);
   thoughtRouter.post('/', thoughtController.create);
